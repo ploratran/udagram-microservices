e95d814f824f125e65a7611e926b68a4024e49a5
Plora Tran <ploratran@Ploras-MacBook.attlocal.net>
2020-06-05 22:21:30 -0700

    commit base code provided from Udacity for Project 4 Udagram

---
+++ udacity-c3-restapi-feed/src/config/config.ts
@@ -0,0 +1,24 @@
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_reigion": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_S3_BUCKET,
+    "url": "http://localhost:8100",
+  },
+  "prod": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET_KEY
+  }
+
+}
